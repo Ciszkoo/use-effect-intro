0848c17aad93a875d56085b9a6cd5bdba89f9af5
Ciszkoo <pawel.olszko.p@gmail.com>
2022-11-20 14:59:11 +0100

    added context

---
+++ src/store/auth-context.js
@@ -0,0 +1,43 @@
+import React, { createContext, useState, useEffect } from "react";
+
+const AuthContext = createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUSerLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUSerLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
